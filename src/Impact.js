59cea761656e5c9328586520c8339a10de2062e1
RodriBlanca <rodrigoblanca96@gmail.com>
2022-02-14 17:17:57 -0300

    First commit

---
+++ src/Impact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// React-router-dom
+import { Routes, Route } from 'react-router-dom';
+
+const Impact = () => {
+  return (
+    <div>Impact</div>
+  )
+}
+
+export default Impact;
